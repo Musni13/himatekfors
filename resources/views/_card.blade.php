7fc750f9b86f6a0354ae9ab280d1542d44d33502
Musni Dewi <musnidewi13@gmail.com>
2025-06-23 17:02:01 +0900

    gambar

--- resources/views/_card.blade.php
+++ resources/views/_card.blade.php
@@ -1,5 +1,5 @@
 <div class="struktur-card mb-4">
-  <img src="{{ asset('assets/template/img/' . $anggota->gambar) }}" alt="{{ $anggota->nama }}" class="img-fluid mb-3 rounded" style="width: 150px; height: 150px; object-fit: cover; border: 3px solid #ccc;">
+  <img src="{{ asset('assets/img/struktur/' . $anggota->gambar) }}" alt="{{ $anggota->nama }}" class="img-fluid mb-3 rounded" style="width: 150px; height: 150px; object-fit: cover; border: 3px solid #ccc;">
   
   <div class="struktur-info">
     <h5 class="mb-1">{{ $anggota->nama }}</h5>
